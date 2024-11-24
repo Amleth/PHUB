08e28e34162165be6ee6279a0c50e67dde013ea3
Thomas Hachem Bottini <thomashachembottini@gmail.com>
2024-11-24 23:35:42 +0100

    .

---
+++ src/renderer/App.tsx
@@ -0,0 +1,20 @@
+import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+
+function Hello() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gray-200">
+      <h1 className="text-blue-500 text-right">Hello Tailwind</h1>
+    </div>
+  );
+}
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Hello />} />
+      </Routes>
+    </Router>
+  );
+}
